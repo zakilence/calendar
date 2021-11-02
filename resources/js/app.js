a321992fe7247ef0a72d04299f1858c1f2c8fc41
zakilence <kosakana11@gmail.com>
2021-11-02 15:01:35 +0900

    app.js コンフリクト解消

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,15 @@
 import Vue from 'vue';
-import Vuex from "vuex"; // vuexライブラリーをimport
 import Vuetify from 'vuetify';// vuetifyをimport
 import Home from "./components/pages/HomeComponent";
-import store from "./store/index"; // vuexストアを読み込む
 
 require('./bootstrap');
 
 Vue.component('HomeComponent', require('./components/pages/HomeComponent.vue').default);
-Vue.use(Vuex);  // Vuexを使用する事を宣言
 Vue.use(Vuetify); //Vuetifyの使用を宣言
 
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
-    store: store,
     components: {
         Home
     }
